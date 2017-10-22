e867894798787e4db9ff970b25f6b977d6488695
aoyagi <aoyagi@ik1-339-29927.vs.sakura.ne.jp>
2017-10-22 15:18:38 +0900

    make it possible to choose date

--- public/schedule.js
+++ public/schedule.js
@@ -5,19 +5,22 @@
     Controllers.controller('ScheduleViewController', ScheduleViewController);
     ScheduleViewController.$inject = ['$scope', '$location', '$http'];
     function ScheduleViewController ($scope, $location, $http) {
+	$scope.cal = new Date();
 
-	const now = moment();
-	const year = now.get('year');
-	const month = now.get('month');
-	const date = now.get('date');
-	$http({
-            method : 'GET',
-            url : `${config.CONTEXT_PATH}/schedule/today`
-        }).success(function(data, status, headers, config) {
-            $scope.schedule = data;
-            console.log(data);
-        }).error(function(data, status, headers, config) {
-            console.log(status);
-        });
+	$scope.$watch('cal', function () {
+	    const m = moment($scope.cal);
+	    const year = m.get('year');
+	    const month = m.get('month') + 1;
+	    const date = m.get('date');
+	    $http({
+		method : 'GET',
+		url : `${config.CONTEXT_PATH}/schedule/${year}/${month}/${date}`
+            }).success(function(data, status, headers, config) {
+		$scope.schedule = data;
+		console.log(data);
+            }).error(function(data, status, headers, config) {
+		console.log(status);
+            });
+	});
     }
 })();
